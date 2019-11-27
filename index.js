ca4d5e6cc79d5e713d2dd64d74216953b23992a8
System Administrator <root@tim.home>
2019-11-27 13:04:46 +0000

    added params

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const Slackbot = require('slackbots');
 const axios = require('axios')
 
 const bot = new Slackbot({
-  token: '',
+  token: 'xoxb-853155594439-839848867779-Xc6dJVyl6xOsdHNiWoSjsmpX',
   name: 'jokebot'
 });
+
+
+// Start Handler
+
+bot.on('start', () => {
+  var params = {
+    icon_emoji: ':cat:'
+  }
+};
